ecd47d6d9002ce6efa8cbd9849449463f73eb195
JakeZook <jakezook01@gmail.com>
2024-01-20 11:24:53 -0700

    Server connection

---
+++ Server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/ ", (req, res) => {
+	res.send("Hello World");
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
